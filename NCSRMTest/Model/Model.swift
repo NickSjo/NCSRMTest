881aa144b70bf7ce2d8365c632b80e5af1a02e80
Niklas S <niklas.c.sjoberg@gmail.com>
2019-08-17 15:48:53 +0200

    Added RESTClient for fetching and parsing data from REST api. Added RESTClient unit tests. Added initial model structure.

---
+++ NCSRMTest/Model/Model.swift
@@ -0,0 +1,39 @@
+//
+//  Model.swift
+//  NCSRMTest
+//
+//  Created by Niklas S on 2019-08-17.
+//  Copyright © 2019 Niklas S. All rights reserved.
+//
+
+import UIKit
+
+struct CharactersResponse: Decodable {
+    var info: Info
+    var results: [Character]
+}
+
+struct Character: Decodable {
+    var id: Int
+    var name: String
+    var origin: Origin
+    var location: Location
+    var image: String
+}
+
+struct Location: Decodable {
+    var name: String
+    var url: String
+}
+
+struct Origin: Decodable {
+    var name: String
+    var url: String
+}
+
+struct Info: Codable {
+    var count: Int
+    var pages: Int
+    var next: String
+    var prev: String
+}
---
+++ NCSRMTestTests/RESTClientTests.swift
@@ -0,0 +1,37 @@
+//
+//  RESTClientTests.swift
+//  NCSRMTestTests
+//
+//  Created by Niklas S on 2019-08-17.
+//  Copyright © 2019 Niklas S. All rights reserved.
+//
+
+import XCTest
+@testable import NCSRMTest
+
+class RESTClientTests: XCTestCase {
+
+    var restClient: RESTClient!
+    
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+        restClient = RESTClient()
+    }
+
+    func testGetAndParseCharactersResponse() {
+        let e = expectation(description: "Test get characters")
+        
+        restClient.performDataTask(with: "https://rickandmortyapi.com/api/character/") { (result: RESTClientResult<CharactersResponse>) in
+            switch result {
+            case .success(let response):
+                XCTAssert(response.results.count > 0)
+                e.fulfill()
+            case .failure:
+                XCTFail()
+            }
+        }
+        
+        waitForExpectations(timeout: 60.0, handler: nil)
+    }
+
+}
