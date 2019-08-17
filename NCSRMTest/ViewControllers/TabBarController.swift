d77a21824d69505904a2bbf34f6b6332f0125e14
Niklas S <niklas.c.sjoberg@gmail.com>
2019-08-17 20:39:24 +0200

    Added tab bar controller as app root view controller.

---
+++ NCSRMTest/ViewControllers/TabBarController.swift
@@ -0,0 +1,27 @@
+//
+//  TabBarController.swift
+//  NCSRMTest
+//
+//  Created by Niklas S on 2019-08-17.
+//  Copyright © 2019 Niklas S. All rights reserved.
+//
+
+import UIKit
+
+class TabBarController: UITabBarController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        
+    }
+
+}
+
+private extension TabBarController {
+    
+    func setupViewControllers() {
+        
+    }
+    
+}
