2b86bbec688c3c2a45fcb245c1b31ecc91820e8d
Niklas S <>
2019-08-17 14:38:41 +0200

    Initial project setup.

---
+++ NCSRMTest/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  NCSRMTest
+//
+//  Created by Niklas S on 2019-08-17.
+//  Copyright © 2019 Niklas S. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
