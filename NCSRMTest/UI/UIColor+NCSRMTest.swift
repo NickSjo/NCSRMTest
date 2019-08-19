eb7c783c49c9adb698c7a038d9f7a38ee8aa1291
Niklas S <niklas.c.sjoberg@gmail.com>
2019-08-19 17:25:56 +0200

    Fix, static custom colors.

--- NCSRMTest/UI/UIColor+NCSRMTest.swift
+++ NCSRMTest/UI/UIColor+NCSRMTest.swift
@@ -9,12 +9,11 @@
 import UIKit
 
 extension UIColor {
-    
-    static var backgroundColor = UIColor(red: 36/255, green: 50/255, blue: 95/255, alpha: 1.0)
-    static var cellSelectionBackgroundColor = UIColor(red: 25/255, green: 40/255, blue: 75/255, alpha: 1.0)
-    static var textColor = UIColor(red: 183/255, green: 228/255, blue: 249/255, alpha: 1.0)
-    static var buttonBackgroundColor = UIColor(red: 166/255, green: 238/255, blue: 230/255, alpha: 1.0)
-    static var buttonTextColor = UIColor(red: 36/255, green: 50/255, blue: 95/255, alpha: 0.7)
-    static var warningColor = UIColor(red: 251/255, green: 100/255, blue: 103/255, alpha: 0.7)
-    static var separatorColor = UIColor(red: 183/255, green: 228/255, blue: 249/255, alpha: 0.7)
+    static let backgroundColor = UIColor(red: 36/255, green: 50/255, blue: 95/255, alpha: 1.0)
+    static let cellSelectionBackgroundColor = UIColor(red: 25/255, green: 40/255, blue: 75/255, alpha: 1.0)
+    static let textColor = UIColor(red: 183/255, green: 228/255, blue: 249/255, alpha: 1.0)
+    static let buttonBackgroundColor = UIColor(red: 166/255, green: 238/255, blue: 230/255, alpha: 1.0)
+    static let buttonTextColor = UIColor(red: 36/255, green: 50/255, blue: 95/255, alpha: 0.8)
+    static let warningColor = UIColor(red: 251/255, green: 100/255, blue: 103/255, alpha: 0.7)
+    static let separatorColor = UIColor(red: 183/255, green: 228/255, blue: 249/255, alpha: 0.7)
 }
