d82a52fded6a8fcce7f64c1d3445dd2f8296b87e
Niklas S <niklas.c.sjoberg@gmail.com>
2019-08-19 17:25:02 +0200

    Added icons to tab bar.

--- NCSRMTest/ViewControllers/TabBarController.swift
+++ NCSRMTest/ViewControllers/TabBarController.swift
@@ -24,12 +24,12 @@ private extension TabBarController {
         let viewController1 = ListTableViewController.instantiate("Main")
         viewController1.viewModel = CharactersListViewModel()
         let navController1 = UINavigationController(rootViewController: viewController1)
-        navController1.tabBarItem = UITabBarItem(title: "Characters", image: nil, tag: 0)
+        navController1.tabBarItem = UITabBarItem(title: "Characters", image: UIImage(named: "icons8-user-male-30"), tag: 0)
         
         let viewController2 = ListTableViewController.instantiate("Main")
         viewController2.viewModel = FavoritesListViewModel()
         let navController2 = UINavigationController(rootViewController: viewController2)
-        navController2.tabBarItem = UITabBarItem(title: "Favorites", image: nil, tag: 1)
+        navController2.tabBarItem = UITabBarItem(title: "Favorites", image: UIImage(named: "icons8-star-30"), tag: 1)
 
         setViewControllers([navController1, navController2], animated: true)
     }
